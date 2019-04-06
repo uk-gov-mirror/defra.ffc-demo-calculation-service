52ff9290aa96795742195ab5ac8844fcb17d97eb
John Watson <johnwatson484@hotmail.com>
2019-04-06 21:36:47 +0100

    Unit tests updated

--- test/index.js
+++ test/index.js
@@ -14,22 +14,11 @@ lab.experiment('API test', () => {
   lab.test('GET / route works', async () => {
     const options = {
       method: 'GET',
-      url: '/'
+      url: '/calculation'
     }
 
     const response = await server.inject(options)
     Code.expect(response.statusCode).to.equal(200)
     Code.expect(response.result).to.equal({ hello: 'world' })
-  })
-
-  lab.test('GET /about route works', async () => {
-    const options = {
-      method: 'GET',
-      url: '/about'
-    }
-
-    const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(200)
-    Code.expect(response.result).to.equal({ ok: 200 })
   })
 })
